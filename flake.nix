79c6fb976ce62fb9c3047e64da67e603a99b1653
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-02 18:45:40 +0200

    Fix Nixtar decryption keys
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I6f03668d3df3f9066a1eaa142c6a8e906a6a6964

--- flake.nix
+++ flake.nix
@@ -91,10 +91,15 @@
                   path_regex = "secrets/(gouv|operator6o|shikanime)\\.enc\\.yaml";
                   key_groups = [
                     {
-                      age = [
-                        "age1pwl9yz4k4255a4h8qz7lafce8wxhsul0cnqwmr8528fqgujlfshshv3z3g" # telsha
-                        "age1x9v4ps90txy9mk4392uya93tyzx40te4dvns4chg5s6q8mfy03ns74jpay" # nixtar
-                      ];
+                      age =
+                        let
+                          telsha = "age1pwl9yz4k4255a4h8qz7lafce8wxhsul0cnqwmr8528fqgujlfshshv3z3g";
+                          nixtar = "age1um232l0h8wn9mtha2qf4f4mnf7ucjayvf5qxjvynatmasg8qg5mshekvjl";
+                        in
+                        [
+                          telsha
+                          nixtar
+                        ];
                     }
                   ];
                 }
